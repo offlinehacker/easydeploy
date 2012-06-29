abd2395a75be7e6cde3d1e7d093913cce025c4c4
Alojzij Blatnik <lojze.blatnik@gmail.com>
2012-06-29 22:24:39 +0200

    apache

---
+++ easydeploy/templates/ubuntu/apache.py
@@ -0,0 +1,20 @@
+from fabric.api import local, settings, abort, run, lcd, cd, put
+from easydeploy.core import state
+
+@state(provides="apache.apache")
+def apache():
+    """
+    Installs apache
+
+    :returns : None
+    :rtype: None
+    """
+    run("apt-get -y install apache2")
+    
+
+    return None
+
+@state(provides="apache.plugin.mod_python", depends="apache.apache")
+def mod_python():
+    
+    return None
