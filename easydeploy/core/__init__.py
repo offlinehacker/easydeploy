f1ccf886f5ffae1153e19a0b5e88ef6423a8a8e4
Jaka Hudoklin <jakahudoklin@gmail.com>
2012-06-29 12:10:49 -0500

    Added docs to core

--- easydeploy/core/__init__.py
+++ easydeploy/core/__init__.py
@@ -5,12 +5,46 @@ from fabric.api import env
 from fabric.contrib.files import upload_template
 
 def err(msg):
+    """
+    Raises Attribute error
+
+    :param msg: Error messasage
+    :type msg: str
+
+    :returns : None
+    :rtype: None
+    """
     raise AttributeError(msg)
 
 def upload_template_jinja2(template, destination, use_sudo=True):
+    """
+    Uploads template using jinja2
+
+    :param template:
+    :type template: a
+    :param destination: a
+    :type destination: a
+    :param use_sudo: a
+    :type use_sudo: a
+
+    :returns : a
+    :rtype: a
+    """
+
     return upload_template(template, destination, context=env, use_sudo=use_sudo, use_jinja=True)
 
 def encpass(key, keypass=None):
+    """
+    Encripts password using key
+
+    :param key: a
+    :type key: a
+    :param keypass: a
+    :type keypass: a
+
+    :returns : a
+    :rtype: a
+    """
     opts = dict (
             keypass=keypass or env.get("keypass") or err("env.keypass is not set")
             )
@@ -20,6 +54,18 @@ def encpass(key, keypass=None):
     RC4.encrypt(key)
 
 def decpass(key, keypass=None):
+    """
+    Decripts password using key
+
+    :param key: Key using to decrypt
+    :type key: str
+    :param keypass: Password using to decript
+    :type keypass: str
+
+    :returns : Plaintext
+    :rtype: str
+    """
+
     opts = dict (
             keypass=keypass or env.get("keypass") or err("env.keypass is not set")
             )
